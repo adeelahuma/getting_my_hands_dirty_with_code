f4522cad026e69436656092465c6feef2216fdd0
Adeela Huma <adeela.huma@gmail.com>
2020-08-12 11:00:17 -0400

    infix | postfix

--- Java/data_structures/src/main/java/StackApplications.java
+++ Java/data_structures/src/main/java/StackApplications.java
@@ -1,10 +1,6 @@
 package main.java;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class StackApplications {
     StackI stackLL = new MyStackLL();
@@ -29,22 +25,205 @@ public class StackApplications {
 
         /////////////////////////////////////////////////////////////
 
-        System.out.println("()  ==> "+ sApplication.balancedParenthesis("()"));
-        System.out.println(")(  ==> "+ sApplication.balancedParenthesis(")("));
-        System.out.println("[()]  ==> "+ sApplication.balancedParenthesis("[()]"));
-        System.out.println("[(])  ==> "+ sApplication.balancedParenthesis("[(])"));
-        System.out.println("[(){}]  ==> "+ sApplication.balancedParenthesis("[(){}]"));
-        System.out.println("[](){}  ==> "+ sApplication.balancedParenthesis("[](){}"));
-        System.out.println("{)}  ==> "+ sApplication.balancedParenthesis("{)}"));
+//        System.out.println("()  ==> "+ sApplication.balancedParenthesis("()"));
+//        System.out.println(")(  ==> "+ sApplication.balancedParenthesis(")("));
+//        System.out.println("[()]  ==> "+ sApplication.balancedParenthesis("[()]"));
+//        System.out.println("[(])  ==> "+ sApplication.balancedParenthesis("[(])"));
+//        System.out.println("[(){}]  ==> "+ sApplication.balancedParenthesis("[(){}]"));
+//        System.out.println("[](){}  ==> "+ sApplication.balancedParenthesis("[](){}"));
+//        System.out.println("{)}  ==> "+ sApplication.balancedParenthesis("{)}"));
+
+
+
+        System.out.println("  ==> "+ sApplication.isBalanced(""));
+        System.out.println("()  ==> "+ sApplication.isBalanced("()"));
+        System.out.println(")(  ==> "+ sApplication.isBalanced(")("));
+        System.out.println("[()]  ==> "+ sApplication.isBalanced("[()]"));
+        System.out.println("[(])  ==> "+ sApplication.isBalanced("[(])"));
+        System.out.println("[(){}]  ==> "+ sApplication.isBalanced("[(){}]"));
+        System.out.println("[](){}  ==> "+ sApplication.isBalanced("[](){}"));
+        System.out.println("{)}  ==> "+ sApplication.isBalanced("{)}"));
+
+
+
+        System.out.println("<<<<<<Infix to Postfix >>>>>>>>>");
+        System.out.println("a+b ==>  " + sApplication.infixToPostfix("a+b"));
+        System.out.println("a+b*c ==>  " + sApplication.infixToPostfix("a+b*c"));
+        System.out.println("a+b*c+d ==>  " + sApplication.infixToPostfix("a+b*c+d"));
+        System.out.println("empty string ==>  " + sApplication.infixToPostfix(""));
+        System.out.println("null string ==>  " + sApplication.infixToPostfix(null));
+        System.out.println("a+b*c/d ==>  " + sApplication.infixToPostfix("a+b*c/d"));
+        System.out.println("a+b-c+d ==>  " + sApplication.infixToPostfix("a+b-c+d"));
+
+
+        System.out.println("<<<<<<<< Evaluate Postfix Notion >>>>>>>>");
+        String exp = "2+2*3";
+        String postfix = sApplication.infixToPostfix(exp);
+        System.out.println(exp + " ==> " + postfix + " ==> "+ sApplication.evaluatePostfix(postfix));
+        exp = "2+2*3+2";
+        System.out.println(exp + " ==> " + sApplication.infixToPostfix(exp) + " ==> "+ sApplication.evaluatePostfix(sApplication.infixToPostfix(exp)));
+
+        System.out.println("null ==> " + sApplication.infixToPostfix(null) + " ==> "+ sApplication.evaluatePostfix(null));
+
 
     }
 
-    void evaluatePostfix(String exp){
+    int evaluatePostfix(String exp){
+
+        List operands = new ArrayList();
+        operands.add("*");
+        operands.add("/");
+        operands.add("+");
+        operands.add("-");
+
+        Stack<String> stack = new Stack<String>();
+
+        if (exp != null && !exp.isEmpty()){
+
+            for (int i = 0 ; i < exp.length(); i++){
+                String curr = String.valueOf(exp.charAt(i));
+
+                if(operands.contains(curr)){
+                    int op1 = Integer.parseInt(stack.pop());
+                    int op2 = Integer.parseInt(stack.pop());
+                    int result = 0;
+
+                    switch (curr){
+                        case "*":
+                            result = op1 *op2;
+                            break;
+                        case "/":
+                            result = op1 / op2;
+                            break;
+                        case "+":
+                            result = op1 + op2;
+                            break;
+                        case "-":
+                            result = op1 - op2;
+                            break;
+                         default:
+                            break;
+                    }
+                    stack.push(Integer.toString(result));
+                }else{
+                    stack.push(curr);
+                }
+            }
+        }
+
+        return stack.isEmpty()? -99 : Integer.parseInt(stack.pop());
+    }
+
+    /**
+     *  Infix to Postfix Notation
+     *
+     *  Assumptions:
+     *      >> Single digit operation
+     *      >> + , -, *, /
+     *
+     *  Test Cases:
+     *  a + b  ==> a b +
+     *  a + b * c  ==> a b c * +
+     *  a + b * c - e  ==> a b c * + e -
+     *  a + b / c  ==> a b c / +
+     *  + b c --> error
+     * */
+
+    String infixToPostfix(String exp){
+        Stack<String>  symbolStack = new Stack<String>();
+
+        Map<String, Integer> operatorOrder = new HashMap<>();
+
+        operatorOrder.put("*", 2);
+        operatorOrder.put("/", 2);
+        operatorOrder.put("+", 1);
+        operatorOrder.put("-", 1);
+
+//        String[] infixExp = new String[exp.length()];
+        StringBuffer postFixExp = new StringBuffer();
+
+
+        if (exp != null && !exp.isEmpty()){
+
+            for (int i = 0 ; i < exp.length(); i++){
+
+                String curr = String.valueOf(exp.charAt(i));
+
+                if (operatorOrder.keySet().contains(curr)){ //operator
+
+
+                    if (!symbolStack.isEmpty()) {
+
+                        String last = symbolStack.peek();
+
+                        //equal precedence ==> pop from top of stcak and use it and push the currenet one
+                        //curr precedence > last then use the curr and p
+
+                        if (operatorOrder.get(curr) > operatorOrder.get(last)) {
+                            symbolStack.push(curr);
+                        } else if (operatorOrder.get(curr) <= operatorOrder.get(last)){
+                            while(!symbolStack.isEmpty())
+                                postFixExp.append(symbolStack.pop());
+
+                            symbolStack.push(curr);
+                        }
+                    }else {
+                        symbolStack.push(curr);
+                    }
+
+                }else {
+                    postFixExp.append(curr);
+                }
+            }
+
+            while(!symbolStack.isEmpty())
+                postFixExp.append(symbolStack.pop());
+
+        }
+
+        return postFixExp.toString();
 
     }
 
-    void infixToPostfix(String exp){
+    boolean isBalanced(String exp){
 
+        String [] openSymbols = {"{", "[", "("};
+        String [] closeSymbols = {"}", "]", ")"};
+
+        Map<String,String> mapping  = new HashMap<String,String>();
+        mapping.put("(", ")");
+        mapping.put("{", "}");
+        mapping.put("[", "]");
+
+
+        Stack<String> stack = new Stack<String>();
+
+        if(exp != null && !exp.isEmpty()){
+
+            for (int i = 0 ; i < exp.length(); i++){
+                String curr = String.valueOf(exp.charAt(i));
+                if (Arrays.asList(openSymbols).contains(curr)){
+                    stack.push(curr);
+                }else if (Arrays.asList(closeSymbols).contains(curr)){
+                    if (stack.isEmpty())
+                        return false;
+
+                   String top = stack.peek();
+
+                   if (curr.equals(mapping.get(top))){
+                       stack.pop();
+                   }
+                   else
+                       return false;
+                }
+            }
+
+
+            if (stack.isEmpty())
+                return true;
+        }
+
+        return false;
     }
 
     boolean balancedParenthesis(String exp){
